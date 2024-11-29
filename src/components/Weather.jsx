a73cefa3590919c891ddeabaa717b3effa857b3d
giselle <gisellesan0801@gmail.com>
2024-11-30 01:13:40 +0800

    Create search feature

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -11,6 +11,7 @@ import humidity_icon from '../assets/humidity.png'
 
 const Weather = () => {
 
+    const inputRef = useRef();
     const [weatherData, setWeatherData] = useState(false);
 
     const allIcons = {
@@ -30,11 +31,21 @@ const Weather = () => {
         "13n": snow_icon
     }
     const search = async (city)=> {
+        if(city === "") {
+            alert("Enter City Name");
+            return;
+        }
         try {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
             const response = await fetch(url);
             const data = await response.json();
+
+            if(!response.ok) {
+                alert(data.message);
+                return;
+            }
+
             console.log(data);
             const icon = allIcons[data.weather[0].icon] || clear_icon;
             setWeatherData({
@@ -45,39 +56,38 @@ const Weather = () => {
                 icon: icon
             })
         } catch (error) {
-
+            setWeatherData(false);
+            console.error("Error in fetching weather data");
         }
     }
-
-    useEffect(()=>{
-        search('Manila')
-    },[])
     
     return (
         <div className="weather">
             <div className="search-bar">
-                <input type="text" placeholder='Search'/>
-                <img src={search_icon} alt="" />
+                <input ref={inputRef} type="text" placeholder='Search'/>
+                <img src={search_icon} alt="" onClick={()=>search(inputRef.current.value)}/>
             </div>
-            <img src={weatherData.icon} alt="" className='weather-icon'/>
-            <p className='temperature'>{weatherData.temperature}°c</p>
-            <p className='location'>{weatherData.location}</p>
-            <div className="weather-data">
-                <div className="col">
-                    <img src={humidity_icon} alt="" />
-                    <div>
-                        <p>{weatherData.humidity}%</p>
-                        <span>Humidity</span>
+            {weatherData?<>
+                <img src={weatherData.icon} alt="" className='weather-icon'/>
+                <p className='temperature'>{weatherData.temperature}°c</p>
+                <p className='location'>{weatherData.location}</p>
+                <div className="weather-data">
+                    <div className="col">
+                        <img src={humidity_icon} alt="" />
+                        <div>
+                            <p>{weatherData.humidity}%</p>
+                            <span>Humidity</span>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <img src={wind_icon} alt="" />
+                        <div>
+                            <p>{weatherData.windSpeed} Km/h</p>
+                            <span>Wind Speed</span>
+                        </div>
                     </div>
                 </div>
-                <div className="col">
-                    <img src={wind_icon} alt="" />
-                    <div>
-                        <p>{weatherData.windSpeed} Km/h</p>
-                        <span>Wind Speed</span>
-                    </div>
-                </div>
-            </div>
+            </>:<></>}
         </div>
     )
 }
