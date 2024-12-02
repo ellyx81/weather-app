ad8d239baf45a56a43626cbf34bdb7f59226a8d0
giselle <gisellesan0801@gmail.com>
2024-12-02 23:34:06 +0800

    start customizing user interface

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -70,48 +70,51 @@ const Weather = () => {
         search("Manila");
     }, []);
 
-    return (
-        <div className="weather">
-            <div className="search-bar">
-                <input
-                    type="text"
-                    value={inputValue}
-                    placeholder="Enter City Name"
-                    onChange={(e) => setInputValue(e.target.value)}
-                    onKeyDown={handleKeyDown}
-                />
-                <img
-                    src={search_icon}
-                    alt="Search"
-                    onClick={() => search(inputValue)}
-                />
+    return (    
+        <>
+            <div className="weather">
+                <div className="search-bar">
+                    <input
+                        type="text"
+                        value={inputValue}
+                        placeholder="Enter City Name"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        onKeyDown={handleKeyDown}
+                    />
+                    <img
+                        src={search_icon}
+                        alt="Search"
+                        onClick={() => search(inputValue)}
+                    />
+                </div>
+                {weatherData ? (
+                    <>
+                        <img src={weatherData.icon} alt="" className="weather-icon" />
+                        <p className="temperature">{weatherData.temperature}°c</p>
+                        <p className="location">{weatherData.location}</p>
+                        <div className="weather-data">
+                            <div className="col">
+                                <img src={humidity_icon} alt="" />
+                                <div>
+                                    <p>{weatherData.humidity}%</p>
+                                    <span>Humidity</span>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <img src={wind_icon} alt="" />
+                                <div>
+                                    <p>{weatherData.windSpeed} Km/h</p>
+                                    <span>Wind Speed</span>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <></>
+                )}
             </div>
-            {weatherData ? (
-                <>
-                    <img src={weatherData.icon} alt="" className="weather-icon" />
-                    <p className="temperature">{weatherData.temperature}°c</p>
-                    <p className="location">{weatherData.location}</p>
-                    <div className="weather-data">
-                        <div className="col">
-                            <img src={humidity_icon} alt="" />
-                            <div>
-                                <p>{weatherData.humidity}%</p>
-                                <span>Humidity</span>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <img src={wind_icon} alt="" />
-                            <div>
-                                <p>{weatherData.windSpeed} Km/h</p>
-                                <span>Wind Speed</span>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            ) : (
-                <></>
-            )}
-        </div>
+            <div className="video"></div>
+        </>
     );
 };
 
